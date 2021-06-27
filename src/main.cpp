4aadf1e07cc24e62bfbc53d37d0d613f2400c1c1
Madjogger1202 <61242548+Madjogger1202@users.noreply.github.com>
2021-06-27 21:17:52 +0300

    added all libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,142 @@
+/*
+            \\----------------------------------------------\\
+            |\\      ------------------------------------    \\\
+            |  \\    -------------------------------           \\\
+            |   ==================================================\
+            |   Hello, it is Mark Gauntlet V5 ultimate code        \
+            |                                                       \
+            |   To make it work properly - be sure, that all         \                   \\\\
+            \\  connected right and libs are edited or downloaded     \             \\\\\   \\\\\\\\\
+           //\\ from this repository   ✓                               \    \\\\\\\\\               \\\\
+         //\\\\\============================================\\\\\\\\\\\\\\\\                           \
+        \\\\\\\\\=======================================================\ ----                          \\
+-------\----   \\\\=====================================================\\ -----------                   \\                       ///////////////////////
+------------     \\\=====================================================\\  - - ---- ----- -----         \\////////////////////// ..........................
+----- \\         \\=======================================================\\\       - -- ---- - -\\\\\\\\\\\\\\\\\\\\\\\\ \\\\\\\\\\\  \...................
+---- \\\         \\\=======================================================\\\                 \\-\\       \\\\\\\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+---- \\\         \\=========================================================\\               __\___\\_________\\\\\\\\\\\\\\\ \\\\\\\\\  \..................
+---- \= =========\\==========================================================\\                     \\          _\\\\\\\\\\\\\\\\\\\\\\\\\..................
+----  ===        \\==========================================================\\                     \\           \\\\\\\\  \\\\\ \\\\\\\ \\\\\\\\\\\\\\\\\\\\\
+----             \\\=========================================================\\     ---             \    \\\\\\\\\\\\\\\\\\\     \  \\\\\\\\.................
+----                                                                              ---           - \\\\\\\\        \\\  \\\\\
+----                                                                             -     ------------\                 \\\\\ \\
+----     ----                                                          -  - ------ -- --- -                            \\\\\\\
+----    -------------------------- - ------------------------------ --    ----  - -                                      \\\ \
+- ---  ---                       ---------              ------------------    --                                         \ \\\\
+--                                     --- --- -- -------                                                                   \
+-------------------------------- -- - -
+
+Now about functional:
+1.Reading data from UI
+  1.1 Getting mode and channel from 2 switches                        
+  1.2 Getting X and Y data from joystick + interpr. into stable values
+  1.3 Getting potentiometr data + filter
+  1.4 Getting buttons data
+  1.5 Checking gesture sensor data 
+  1.6 Getting data from both flex. sensors
+  1.7 Getting data from EMG sensor
+
+2. Reading from sensors to determine the position
+  2.1 Reading from magnetometer
+  2.2 Reading from both acselerometers
+  2.3 Reading from both gyroscopes
+  2.4 Getting coordinates from GPS module
+  2.5 Getting altitude from pressure sensor 
+
+3. Getting data about enviroment and general stuff
+  3.1 Read data from thermometer
+  3.2 Read data from humid. sensor
+  3.3 Read data from tVOC sensor
+  3.4 Read data from O2 sensor
+  3.5 Read data from CO2 sensor 
+  3.6 Getting real time from RTC module
+  3.7 Getting commands from voice recognition module 
+
+4. Sending data through wireless com. devices
+  4.1 Sending data via NRF24L01+ (100 mW)
+  4.2 Sending data via LoRa module 
+  4.3 Sending data via bluetooth module
+  4.4 Sending data via ESP8266 (WiFi)
+
+5. UI and cool features
+  5.1 Menu on display + touchscreen response
+  5.2 Patterns of illumination of the address LED strip
+  5.3 Two-step activation
+  5.4 The ability to play music and any replicas
+
+*/
+
+
 #include <Arduino.h>
+
+#include <SPI.h>
+
+#include <Wire.h>
+
+#include <SoftwareSerial.h>
+
+#include "Adafruit_GFX.h"
+#include <MCUFRIEND_kbv.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_MPU6050.h>
+
+#include "microLED.h"
+
+#include <LoRa.h>
+
+#include "nRF24L01.h"
+#include "RF24.h"
+#include "printf.h"
+
+#include <iarduino_GPS_NMEA.h>
+
+#include <iarduino_RTC.h>
+
+#include <Adafruit_BME280.h>
+
+#include "Adafruit_APDS9960.h"
+
+#include <SHT3x.h> 
+
+#include "Adafruit_SGP30.h"
+
+#include "VoiceRecognitionV3.h"
+
+#include <DFPlayer_Mini_Mp3.h>
+
+MCUFRIEND_kbv tft;
+
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+Adafruit_MPU6050 mpu;
+
+#define NUM_LEDS 8  // кол-во диодов
+#define LED_PIN 8   // пин подключения
+
+#define REPLACE_FASTLED // пункт 0
+#define COLOR_DEBTH 3   // пункт 1
+LEDdata leds[NUM_LEDS];
+
+microLED strip(leds, NUM_LEDS, LED_PIN);
+
+RF24 radio(46, 53);
+
+iarduino_GPS_NMEA gps; 
+
+iarduino_RTC watch(RTC_DS1307); 
+
+Adafruit_BME280 bme;
+
+Adafruit_APDS9960 apds;
+
+SHT3x SHT; 
+
+Adafruit_SGP30 sgp;
+
+VR myVR(2,3);
+
 
 #define LCD_CS A3 // Chip Select goes to Analog 3
 #define LCD_CD A2 // Command/Data goes to Analog 2
@@ -20,12 +158,6 @@ MCUFRIEND_kbv tft;
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 #define BLUEWHITE   0xEFFF
-
-#ifndef min
-#define min(a, b) (((a) < (b)) ? (a) : (b))
-#endif
-
-uint16_t g_identifier;
 
 
 void setup() {
